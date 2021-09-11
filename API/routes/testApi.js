643820e5eeea6c03378dedd50cfcb0af93530e0f
Shivam Kumar Singh <80257500+shivamXoogle@users.noreply.github.com>
2021-09-11 15:57:33 +0530

    Update testApi.js

--- API/routes/testApi.js
+++ API/routes/testApi.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 
 router.get('/', async(req, res) => {
     let dataObj = {
-        "text":"test"
+        "text":"update This Field",
+        "New Key":"New Value"
     };
     return res.status(200).json(dataObj);
 });
