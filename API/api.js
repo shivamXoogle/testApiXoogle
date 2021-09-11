78e951d407d4278c48a6136b2fbbd67af7c17592
Shivam Singh <shivam@xoogle.in>
2021-09-11 15:48:13 +0530

    Updated API

--- API/api.js
+++ API/api.js
@@ -1,13 +1,6 @@
 const jwt = require('../config/jwt');
 const app = require('express')();
 
-// public
-app.use('/auth', require('./routes/auth'));
-
-// private
-app.use('/employees', jwt.verify, require('./routes/employees'));
-app.use('/stations', jwt.verify, require('./routes/stations'));
-app.use('/bills', jwt.verify, require('./routes/bills'));
 app.use('/testApi', require('./routes/testApi'));
 
 module.exports = app;
